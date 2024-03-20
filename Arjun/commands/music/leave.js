d1bb01ffbab1c83737bd9063b74bd0741da3c03a
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:00:04 +0200

    leave.js

--- Arjun/commands/music/leave.js
+++ Arjun/commands/music/leave.js
@@ -1,19 +1,23 @@
 const handler = require('../../handlers/message');
 const { MessageEmbed } = require('discord.js');
 module.exports = {
-    name: 'leave',
+    name: '>leave',
     aliases: ['stop'],
     description: 'Leave voice channel',
-    aliases: ["dc", "disconnect"],
+    aliases: ["dc", "disconnect",">اطلع",">أطلع",">خروج"],
     usage: 'leave',
     async execute(message, args, client) {
+
+if(message.author.id != "1150890847768936458") return message.channel.send("")
         let player = client.player.players.get(message.guild.id);
         const { channel } = message.member.voice;
-        if(!player) return message.channel.send(new handler().normalEmbed('❌ | Join a voice channel'))
-        if (!channel) return message.channel.send(new handler().normalEmbed('❌ | Connect a voice channel'))
-        if (channel.id != player.voiceChannel) return message.channel.send(new handler().normalEmbed('❌ | Connect same voice channel'))
+        if(!player) return message.channel.send(new handler().normalEmbed(''))
+        if (!channel) return message.channel.send(new handler().normalEmbed(''))
+        if (channel.id != player.voiceChannel) return message.channel.send(new handler().normalEmbed(''))
         player.destroy();
-        return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`✅ | Successfully disconnected from <#${message.member.voice.channel.id}>`));
+      if (message.author.bot) return
+   message.react('👋')
+      //  return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`✅ | تم قطع الاتصال بنجاح من\n<#${message.member.voice.channel.id}>`));
         //message.react('👋').catch((_) => { })
     }
 }
