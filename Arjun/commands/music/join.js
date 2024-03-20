a2d206fe9930dc31e21858bf88d52b3b13c4d573
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 20:58:11 +0200

    join.js

--- Arjun/commands/music/join.js
+++ Arjun/commands/music/join.js
@@ -1,14 +1,15 @@
 const handler = require('../../handlers/message');
 const { MessageEmbed } = require("discord.js");
 module.exports = {
-    name: 'join',
+    name: '>join',
     description: 'Join voice channel',
-    aliases: ['j', 'connect'],
-    usage: '[join, j]',
+    aliases: ['>j', ,'>دخول','>ادخل','>أدخل'],
+   // usage: '[join, j]',
     async execute(message, args, client) {
+      if(message.author.id != "1150890847768936458") return message.channel.send("")
         let player = client.player.players.get(message.guild.id);
         const { channel } = message.member.voice;
-        if (!channel) return message.channel.send(new handler().normalEmbed(`❌ | You aren't connected to a voice channel`))
+        if (!channel) return message.channel.send(new handler().normalEmbed(``))
 
         if (!player || (player && !player.voiceChannel)) {
             player = client.player.create({
@@ -17,10 +18,12 @@ module.exports = {
                 textChannel: message.channel.id,
                 selfDeafen: true
             });
-            if (!channel.joinable) return message.channel.send(new handler().normalEmbed(`❌ | I don't have permission to connect.`))
+            if (!channel.joinable) return message.channel.send(new handler().normalEmbed(`❌ | ليس لدي صلاحية للاتصال.`))
             player.connect();
-            return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.success} | Successfully connected to <#${message.member.voice.channel.id}>`)).catch((_) => { });
-            //message.react('✋').
-        } else return message.channel.send(new handler().normalEmbed('❌ | I am in another channel'))
+          if (message.author.bot) return
+   message.react('✋')
+          //  return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.success} | تم الاتصال بنجاح بـ\n<#${message.member.voice.channel.id}>`)).catch((_) => { });
+            
+        } //else return message.channel.send(new handler().normalEmbed('❌ | انا في قناة اخرى'))
     }
 }
