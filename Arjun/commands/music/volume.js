67b6898354fd079e545f7b01ea31b68ec75cbb52
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:30:02 +0200

    volume.js

--- Arjun/commands/music/volume.js
+++ Arjun/commands/music/volume.js
@@ -5,18 +5,23 @@ module.exports = {
     name: 'volume',
     description: 'Set volume of the player',
     usage: 'volume [ value ]',
-    aliases: ['v', 'vol'],
+    aliases: ['v', 'vol','صوت','الصوت'],
     async execute(message, args, client) {
         try {
             const player = client.player.players.get(message.guild.id);
-            if (!player) return message.channel.send(new handler().normalEmbed('❌ | Currently not playing anything.'))
-            if (!args[0]) return message.channel.send(new handler().normalEmbed(`🔉 | Volume is at \`${player.volume}%.\``))
-            if (isNaN(args[0])) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | You can set volume 0 to 150`));
-            if (args[0] < 0 || args[0] > 150) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | You can set volume 0 to 150`));
+          if (!message.member.voice.channel)
+                return message.channel.send(new handler().normalEmbed(''))
+          
+            if (!player) return message.channel.send(new handler().normalEmbed(''))
+            if (!args[0]) return message.channel.send(new handler().normalEmbed(`🔉 | حجم الصوت الأن: \`${player.volume}%.\``))
+            if (isNaN(args[0])) return message.channel.send(new handler().normalEmbed(``));
+            if (args[0] < 0 || args[0] > 150) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | | تستطيع تحديد حجم الصوت من **0** إلى **150**`));
             player.setVolume(parseInt(args[0]))
-            return message.channel.send(new handler().normalEmbed('🔉 | Volume set to \`' + args[0] + '%.\`'))
+          if (message.author.bot) return
+   message.react('🔊')
+            return message.channel.send(new handler().normalEmbed('🔉 | تم ضبط مستوى الصوت على: \`' + args[0] + '%.\`'))
         } catch (err) {
-            message.channel.send(new handler().normalEmbed(`❌ | Oops, there was an error! ` + err))
+            message.channel.send(new handler().normalEmbed(``))
         }
     }
 };
