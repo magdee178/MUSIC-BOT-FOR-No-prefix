0630b9282df47967ca6fe2796128be6844682628
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:23:07 +0200

    seek.js

--- Arjun/commands/music/seek.js
+++ Arjun/commands/music/seek.js
@@ -6,18 +6,18 @@ module.exports = {
     name: 'seek',
     description: 'Seeks to a certain point in the current track.',
     usage: 'seek <10>',
-    aliases: [''],
+    aliases: ['تقديم'],
     async execute(message, args, client, prefix) {
       const { channel } = message.member.voice;
-        if (!channel) return message.channel.send(new handler().normalEmbed(`❌ | You aren't connected to a voice channel`))
-     if(isNaN(args[0])) return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.error} | Invalid number. Please provide a number in seconds.\nCorrect Usage: \`${prefix}seek <seconds>\``));
+        if (!channel) return message.channel.send(new handler().normalEmbed(``))
+     if(isNaN(args[0])) return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.error} | رقم غير صالح. يرجى تقديم رقم في ثوان.\nطريقة الإستخدام: \`${prefix}seek <seconds>\``));
 		const player = client.player.players.get(message.guild.id);
-		if(args[0] * 1000 >= player.queue.current.duration || args[0] < 0) return message.channel.send(`${client.emoji.error} | Cannot seek beyond length of song.`);
+		if(args[0] * 1000 >= player.queue.current.duration || args[0] < 0) return message.channel.send(`${client.emoji.error} | لا يمكن البحث عن ما يتجاوز طول الأغنية.`);
 		player.seek(args[0] * 1000);
 
-    if (!player) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | You aren't connected to a voice channel type ${prefix}join or ${prefix}play`))
+    if (!player) return message.channel.send(new handler().normalEmbed(``))
 
 		const parsedDuration = format(player.position);
-		return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.success} | Seeked to ${parsedDuration}`));
+		return message.channel.send(new MessageEmbed().setColor(client.colors.main).setDescription(`${client.emoji.success} | تقديم إلى: ${parsedDuration}`));
 	}
- }
+}
