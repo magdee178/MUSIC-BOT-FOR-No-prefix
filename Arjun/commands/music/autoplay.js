3b369edd9d431526fb0daef8d43ca686b7e3f551
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 20:53:07 +0200

    autoplay.js

--- Arjun/commands/music/autoplay.js
+++ Arjun/commands/music/autoplay.js
@@ -4,7 +4,7 @@ const handler = require('../../handlers/message');
 
 module.exports = {
     name: "autoplay",
-    aliases: ["ap"],
+    aliases: ["ap","تشغيل_تلقائي","التشغيل_التلقائي","اوتو_بلاي","أوتو_بلاي","اوتوبلاي","أوتوبلاي"],
 	category: "Music",
     description: "Toggle music autoplay",
     args: false,
@@ -22,7 +22,7 @@ module.exports = {
         if (!player.queue.current) {
             let thing = new MessageEmbed()
                 .setColor(client.colors.error)
-                .setDescription(`${client.emoji.error} | Currently not playing anything.`);
+                .setDescription(`${client.emoji.error} | حالياً لا توجد أغنية تعمل!`);
             return message.channel.send(thing);
         }
 
@@ -33,6 +33,8 @@ module.exports = {
         if (autoplay === false) {
             const identifier = player.queue.current.identifier;
             player.set("autoplay", true);
+     if (message.author.bot) return
+   message.react('✅')
             player.set("requester", message.author);
             player.set("identifier", identifier);
             const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
@@ -40,16 +42,19 @@ module.exports = {
             player.queue.add(res.tracks[1]);
             let thing = new MessageEmbed()
                 .setColor(client.colors.main)
+    //.setThumnail(`${client.user.displayAvatarURL()}`)
                 
-                .setDescription(`${client.emoji.success} | Autoplay is now \`enabled\` in this server.`)
+                .setDescription(`**تم التشغيل , استمتع**`)
            return message.channel.send(thing);
         } else {
             player.set("autoplay", false);
+          if (message.author.bot) return
+   message.react('❌')
             player.queue.clear();
             let thing = new MessageEmbed()
                 .setColor(client.colors.main)
                 
-                .setDescription(`${client.emoji.success} | Autoplay is now \`disabled\` in this server.`)
+                //.setDescription(`${client.emoji.success} | وضع **التشغيل التلقائي** الأن\`معطّل\` في هذا السيرفر!`)
                
             return message.channel.send(thing);
         }
