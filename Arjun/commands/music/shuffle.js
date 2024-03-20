0f38ccb2d1d85be89d2f07218d530a24fbc6e070
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:24:08 +0200

    shuffle.js

--- Arjun/commands/music/shuffle.js
+++ Arjun/commands/music/shuffle.js
@@ -3,16 +3,18 @@ const handler = require('../../handlers/message');
 module.exports = {
     name: 'shuffle',
     description: 'Shuffle the song in queue',
-    aliases: ["sf"],
+    aliases: ["sf","خلط"],
     usage: 'shuffle',
     async execute(message, args, client) {
         const player = client.player.players.get(message.guild.id);
-        if (!player) return message.channel.send(new handler().normalEmbed('❌ | Currently not playing anything.'))
-        if (!player.queue.current) return message.channel.send(new handler().normalEmbed('❌ | Currently not playing anything.'))
-        if (player.queue.size == 0) return message.channel.send(new handler().normalEmbed('❌ | Not enough song to shuffle'))
+        if (!player) return message.channel.send(new handler().normalEmbed(''))
+        if (!player.queue.current) return message.channel.send(new handler().normalEmbed(''))
+        if (player.queue.size == 0) return message.channel.send(new handler().normalEmbed('❌ | لا توجد أغاني كافية للخلط'))
         player.queue.shuffle()
+      if (message.author.bot) return
+   message.react('🔀')
         await client.playerHandler.savePlayer(client.player.players.get(message.guild.id))
-        message.channel.send(new handler().normalEmbed(`${client.emoji.success} | Shuffled the current queue.`))
+        message.channel.send(new handler().normalEmbed(`${client.emoji.success} | خلط قائمة الانتظار الحالية.`))
         //message.react('🔀').catch((_) => { })
     }
 }
