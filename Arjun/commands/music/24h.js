4a59405ce0d9f2217a90155d43f0807340d8f0b0
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 20:51:20 +0200

    24h.js

--- Arjun/commands/music/24h.js
+++ Arjun/commands/music/24h.js
@@ -7,19 +7,22 @@ const autoJoin = require('../../schemas/autoJoin');
 module.exports = {
     name: '24/7',
     description: 'bot whether to leave vc when there\'s user inside vc or not',
-    aliases: ['247', '24*7', '24-7'],
-    usage: '247',
+    aliases: ['>247','>24/7','>sc'],
+   // usage: '247',
     async execute(message, args, client, prefix) {
+      if(message.author.id != "1150890847768936458") return message.channel.send("")
     //if (!message.guild.me.hasPermission("MANAGE_GUILD"))
-    if (!message.member.hasPermission(["MANAGE_GUILD"]))
-    return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | You don't have \`MANAGE_GUILD\` permission for this command!`));
+    //if (!message.member.hasPermission(["MANAGE_GUILD"]))
+  // return message.channel.send(new handler().normalEmbed(`${client.emoji.error} | انت لا تملك صلاحية: \`MANAGE_GUILD\` لإستخدام هذا الأمر!`));
 
     const player = client.player.players.get(message.guild.id);
-    if (!player) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} |  You aren't connected to a voice channel, type ${prefix}join or ${prefix}play`))
+    //if (!player) return message.channel.send(new handler().normalEmbed(`${client.emoji.error} |  أنت غير متصل بقناة صوتية, اكتب:\n\`${prefix}join\` او \`${prefix}play\``))
 
     if (player.get('24h').status == false) {
       player.set('24h', { status: true })
-      message.channel.send(new handler().normalEmbed(`${client.emoji.success} | 24/7 mode is now \`enabled\` in this server.`))
+     // message.channel.send(new handler().normalEmbed(`${client.emoji.success} | وضع 24/7 الأن \`مفعّل\` في هذا السيرفر!`))
+      if (message.author.bot) return
+   message.react('✅')
 
     await autoJoin.findOneAndDelete({
       guildID: message.guild.id,
@@ -41,7 +44,9 @@ module.exports = {
       if (vcdata) {
         await vcdata.delete();
 	    player.set('24h', { status: false })
-      message.channel.send(new handler().normalEmbed(`✅ | 24/7 mode is now \`disabled\` in this server.`))
+     // message.channel.send(new handler().normalEmbed(`✅ | وضع 24/7 الأن \`معطّل\` في هذا السيرفر!`))
+        if (message.author.bot) return
+   message.react('❌')
     }
   }
 }
