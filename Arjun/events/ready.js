a4df36fac71fef8536561a7689e9b80ab74ad36e
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:33:29 +0200

    ready.js

--- Arjun/events/ready.js
+++ Arjun/events/ready.js
@@ -9,14 +9,15 @@ module.exports = {
         const player = new lavalink(client);
         client.player = player;
         player.init(client.user.id);
-        client.user.setActivity(`@${client.user.username}`, {type: "LISTENING", url: "https://www.twitch.tv/nanotect_" }), 15000;
-        /*setInterval(() => {
+        client.user.setActivity(`ZAX Music Pro`, {type: "STREAMING", url: "https://www.twitch.tv/4egy" }), 15000;
+      /*  setInterval(() => {
             let statusList = [
-                ``
+                `ZAX Music`,
+              `Dev By MaGdEe🛠️`
                 
             ]
             let choosenStatus = statusList[Math.round(Math.random() * statusList.length)]
-            client.user.setActivity(`@Disney`, { type : "LISTENING" })
+            client.user.setActivity(`Egypt`, { type : "PLAYING" })
         }, 40 * 1000);*/
         console.log(chalk.green(`[CLIENT] => [READY] ${client.user.tag} is now ready!`))
         await Util.delayFor(800)
