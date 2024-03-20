d6febcb133e950b6f2fd0d8c87a48f80c9fcb795
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 22:04:22 +0200

    index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('TEC | Basanti Music'))
+app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () =>
+/*app.listen(port, () =>
 console.log(`TEC !!!! Your app is listening a http://localhost:${port}`)
-);
+);*/
 
 const chalk = require('chalk');
 require('dotenv').config()
 require('./Arjun/modules/console');
-console.log(chalk.yellow('----------------------------------------------------------------------------------------------'))
+//console.log(chalk.yellow('----------------------------------------------------------------------------------------------'))
 console.log()
-console.log(chalk.red('TEC !!!! Found a bug? Feel free to join our server'))
-console.log(chalk.yellow('TEC | Version: 3'))
+console.log(chalk.red('Develepor By: MaGdEe#0001🛠️'))
+//console.log(chalk.yellow('TEC | Version: 3'))
 console.log()
-console.log(chalk.yellow('----------------------------------------------------------------------------------------------'))
+//console.log(chalk.yellow('----------------------------------------------------------------------------------------------'))
 console.log()
 const Alpine = require('./Arjun/Arjun');
 
