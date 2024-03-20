c96ca313ec4674fc41a1e5f586e97a216278e6a1
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:01:42 +0200

    loop.js

--- Arjun/commands/music/loop.js
+++ Arjun/commands/music/loop.js
@@ -4,15 +4,16 @@ module.exports = {
     name: 'loop',
     description: 'Loop the player',
     usage: 'loop',
-    aliases: ['l'],
+    aliases: ['l','تكرار'],
     async execute(message, args, client) {
         const player = client.player.players.get(message.guild.id);
-        if (!player) return message.channel.send(new handler().normalEmbed(`❌ | Currently not playing anything.`))
-        if (!player.queue.current) return message.channel.send(new handler().normalEmbed('❌ | Currently not playing anything.'))
+        if (!player) return message.channel.send(new handler().normalEmbed(``))
+        if (!player.queue.current) return message.channel.send(new handler().normalEmbed(''))
         player.toggleLoop()
             .then(async x => {
                 await client.playerHandler.savePlayer(client.player.players.get(message.guild.id))
-                return message.channel.send(new handler().normalEmbed(`✅ | Loop \`enabled\`  ${x.status}`))
+              
+                return message.channel.send(new handler().normalEmbed(` ✅ | تم \`تفعيل\` وضع **التكرار** على: __${x.status}__`))
             })
             .catch(err => {
                 message.channel.send(new handler().normalEmbed(err))
