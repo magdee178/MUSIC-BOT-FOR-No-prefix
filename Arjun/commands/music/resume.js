fce12a0739c6720a74c9c562c4ad9c3d734d05aa
magdee178 <150984583+magdee178@users.noreply.github.com>
2024-03-20 21:21:54 +0200

    resume.js

--- Arjun/commands/music/resume.js
+++ Arjun/commands/music/resume.js
@@ -3,16 +3,18 @@ const handler = require('../../handlers/message');
 module.exports = {
     name: "resume",
     description: "Resume the player",
-    aliases: ['r'],
-    usage: "resume",
+    aliases: ['r','تشغيل'],
+  //  usage: "resume",
     async execute(message, args, client) {
         const player = client.player.players.get(message.guild.id)
-        if (!player) return message.channel.send(new handler().normalEmbed(`❌ | Currently not playing anything.`))
+        if (!player) return message.channel.send(new handler().normalEmbed(``))
         const { channel } = message.member.voice
-        if (!channel) return message.channel.send(new handler().normalEmbed(`❌ | You aren't connected to a voice channel`))
-        if (player && (channel.id != player?.voiceChannel)) return message.channel.send(new handler().normalEmbed(`❌ | You're not in the same voice channel.`))
+        if (!channel) return message.channel.send(new handler().normalEmbed(``))
+        if (player && (channel.id != player?.voiceChannel)) return message.channel.send(new handler().normalEmbed(``))
         player.pause(false);
-        message.channel.send(new handler().normalEmbed(`${client.emoji.success} | Resumed the current song.`));
+      if (message.author.bot) return
+   message.react('⏯️')
+        message.channel.send(new handler().normalEmbed(`${client.emoji.success} | استأنفت الأغنية الحالية.`));
         //message.react('<a:Tick:946432888084254730>').catch((_) => { })
     }
 };
